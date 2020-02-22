606db9065293a7343ed36e683174acf5bf2553c0
flavio10araujo <flavio10araujo@yahoo.com.br>
2020-02-22 15:54:13 -0500

    Fibonnaci with Dynamic Programming.

--- src/General/Fibonacci.java
+++ src/General/Fibonacci.java
@@ -2,7 +2,16 @@ package General;
 // Fibonacci Series using Recursion.
 // Ex.: 0 1 1 2 3 5 8 13 = the next will be 21.
 class Fibonacci {
-	static int fib(int n) {
+	public static void main(String args[]) {
+		int n = 8;
+		System.out.println(fibSolution01(n));
+		//System.out.println(fibSolution02(n));
+	}
+	
+	/**
+	 * Solução usando recursividade.
+	 */
+	private static int fibSolution01(int n) {
 		
 		System.out.println("n = " + n);
 		
@@ -11,11 +20,32 @@ class Fibonacci {
 			return n;
 		}
 		
-		return fib(n - 1) + fib(n - 2);
+		return fibSolution01(n - 1) + fibSolution01(n - 2);
 	}
+	
+	/**
+	 * Solução usando Dynamic Programming.
+	 * Essa solução é melhor que a 01 porque evita de refazer um cálculo que já foi feito.
+	 */
+	private static int fibSolution02(int n) {
+	    int memoize[] = new int[n+1];
+	    return CalculateFibonacciRecursive(memoize, n);
+	  }
+	
+	private static int CalculateFibonacciRecursive(int[] memoize, int n) {
+		
+		System.out.println("n = " + n);
+		
+		if (n < 2) {
+			System.out.println("entrou no if");
+			return n;
+		}
 
-	public static void main(String args[]) {
-		int n = 8;
-		System.out.println(fib(n));
-	}
+	    // if we have already solved this subproblem, simply return the result from the cache
+	    if(memoize[n] != 0)
+	      return memoize[n];
+
+	    memoize[n] = CalculateFibonacciRecursive(memoize, n-1) + CalculateFibonacciRecursive(memoize, n-2);
+	    return memoize[n];
+	  }
 }
