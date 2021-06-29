56b29121bbe951f1d258387509fc2e60ead613de
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-06-28 20:09:45 -0400

    OK

--- src/SortingAlgorithm/QuickSort.java
+++ src/SortingAlgorithm/QuickSort.java
@@ -3,26 +3,27 @@ package SortingAlgorithm;
 public class QuickSort {
 
 	/*
-	 * This function takes last element as pivot, places the pivot element at
-	 * its correct position in sorted array, and places all smaller (smaller
-	 * than pivot) to left of pivot and all greater elements to right of pivot
+	 * This function takes last element as pivot, 
+	 * places the pivot element at its correct position in sorted array, 
+	 * and places all smaller (smaller than pivot) to left of pivot 
+	 * and all greater elements to right of pivot.
 	 */
 	int partition(int arr[], int low, int high) {
 		int pivot = arr[high];
-		int i = (low - 1); // index of smaller element
+		int i = (low - 1); // Index of smaller element.
+		
 		for (int j = low; j < high; j++) {
-			// If current element is smaller than the pivot
+			// If current element is smaller than the pivot.
 			if (arr[j] < pivot) {
 				i++;
-
-				// swap arr[i] and arr[j]
+				// Swap arr[i] and arr[j].
 				int temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
 			}
 		}
 
-		// swap arr[i+1] and arr[high] (or pivot)
+		// Swap arr[i + 1] and arr[high] (or pivot).
 		int temp = arr[i + 1];
 		arr[i + 1] = arr[high];
 		arr[high] = temp;
@@ -30,29 +31,20 @@ public class QuickSort {
 		return i + 1;
 	}
 
-	/*
-	 * The main function that implements QuickSort() arr[] --> Array to be
-	 * sorted, low --> Starting index, high --> Ending index
-	 */
-	void sort(int arr[], int low, int high) {
-		
+	void sort(int arr[], int low, int high) {	
 		if (low < high) {
 			
 			printArray(arr);
 			
-			/*
-			 * pi is partitioning index, arr[pi] is now at right place
-			 */
+			// pi is partitioning index, arr[pi] is now at right place.
 			int pi = partition(arr, low, high);
 
-			// Recursively sort elements before
-			// partition and after partition
+			// Recursively sort elements before partition and after partition.
 			sort(arr, low, pi - 1);
 			sort(arr, pi + 1, high);
 		}
 	}
 
-	/* A utility function to print array of size n */
 	static void printArray(int arr[]) {
 		int n = arr.length;
 		for (int i = 0; i < n; ++i)
@@ -60,11 +52,10 @@ public class QuickSort {
 		System.out.println();
 	}
 
-	// Driver program
 	public static void main(String args[]) {
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90, 1, 29 };
-		System.out.println("Quick Sort:");
 		QuickSort ob = new QuickSort();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 1 };
+		System.out.println("Quick Sort:");
 		ob.sort(arr, 0, arr.length - 1);
 		printArray(arr);
 	}
