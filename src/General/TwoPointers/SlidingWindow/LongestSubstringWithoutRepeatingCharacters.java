83409f7706ceb33351ba8438f1f83ded7b4ead68
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-07-06 20:30:34 -0400

    OK

---
+++ src/General/TwoPointers/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,43 @@
+package General.TwoPointers.SlidingWindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Find the length of the longest substring of a given string without repeating characters.
+ * Input: abccabcabcc
+ * Output: 3
+ * Explanation: longest substrings are abc, cab, both of length 3.
+ * 
+ * Input: aaaabaaa
+ * Output: 2
+ * Explanation: ab is the longest substring, length 2
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+	
+	public static int longestSubstringWithoutRepeatingCharacters(String s) {    
+		int n = s.length();
+        int longest = 0, l = 0, r = 0;
+        Set<Character> window = new HashSet<>();
+        
+        while (r < n) {
+            if (!window.contains(s.charAt(r))) {
+                window.add(s.charAt(r));
+                r++;
+            } else {
+                window.remove(s.charAt(l));
+                l++;
+            }
+            
+            longest = Math.max(longest, r - l);
+        }
+        
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcdbea";
+        int res = longestSubstringWithoutRepeatingCharacters(s);
+        System.out.println(res);
+    }
+}
