5765261a45a3fe54b92e742da2a1f0a559e7b641
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-08-02 20:35:07 -0400

    OK

---
+++ src/General/Graph/BFS/ZombieInMatrix.java
@@ -0,0 +1,134 @@
+package General.Graph.BFS;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * You are given an m by n grid of integers representing a map of a dungeon. 
+ * In this map:
+ * 		-1 represents a wall or an obstacle of some kind.
+ * 		0 represents a gate out of the dungeon.
+ * 		INF represents empty space.
+ * 			For this question, let INF be 2^31 - 1 == 2147483647, which is the max value of the integer type in many programming languages.
+ * 
+ * Venturing into the dungeon is very dangerous, so you would like to know how close you are at each point in the dungeon to the exit. 
+ * Given the map of the dungeon, return the same map, but for each empty space, 
+ * that space is replaced by the number of steps it takes to reach any exit. 
+ * If a space cannot reach the exit, that space remains INF.
+ * 
+ * Note that each step, you can move horizontally or vertically, but you cannot move pass a wall or an obstacle.
+ * 
+ * Input
+ * 		dungeon_map: a matrix of integer representing the dungeon map.
+ * Output
+ * 		A matrix of integer representing the dungeon map with the addition of distance to an exit for each empty space.
+ * 
+ * Input:
+ * dungeon_map = [
+ * 		[INF,  -1,   0, INF],
+ * 		[INF, INF, INF,  -1],
+ * 		[INF,  -1, INF,  -1],
+ * 		[  0,  -1, INF, INF],
+ * ]
+ * Output: [ 
+ * 		[3  ,  -1,   0,   1], 
+ * 		[2  ,   2,   1,  -1], 
+ * 		[1  ,  -1,   2,  -1], 
+ * 		[0  ,  -1,   3,   4], 
+ * ]
+ * 
+ * Constraints: 1 <= n, m <= 500
+ * 
+ * Solution:
+ * 
+ * This is a classical breadth-first search problem, as it asks for the distance from each point to some location. 
+ * Simply initialize the queue with a list of gate locations, and each cycle, when we process the location at the front of the queue, 
+ * we add all the adjacent locations into the queue if their value is INF (meaning it is empty and unprocessed) 
+ * and mark the distances on the cells by adding the value of the current cell by 1. 
+ * This way each empty space is only in the queue once.
+ * The time complexity is O(n * m).
+ */
+public class ZombieInMatrix {
+
+	public static class Coords {
+        public int row;
+        public int col;
+
+        public Coords(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public Coords add(Coords other) {
+            return new Coords(row + other.row, col + other.col);
+        }
+    }
+
+    public static List<Coords> directions = List.of(new Coords(0, 1), new Coords(1, 0), new Coords(0, -1), new Coords(-1, 0));
+
+    public static List<List<Integer>> mapGateDistances(List<List<Integer>> dungeonMap) {
+        Deque<Coords> deque = new ArrayDeque<>();
+        int n = dungeonMap.size(), m = dungeonMap.get(0).size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dungeonMap.get(i).get(j) == 0) {
+                    deque.offer(new Coords(i, j));
+                }
+            }
+        }
+        
+        while (!deque.isEmpty()) {
+            Coords currentPos = deque.poll();
+            
+            for (Coords delta : directions) {
+                Coords newPos = currentPos.add(delta);
+                
+                if (newPos.row >= 0 && newPos.row < n && newPos.col >= 0 && newPos.col < m) {
+                    if (dungeonMap.get(newPos.row).get(newPos.col) == Integer.MAX_VALUE) {
+                        dungeonMap.get(newPos.row).set(newPos.col, 1 + dungeonMap.get(currentPos.row).get(currentPos.col));
+                        deque.add(newPos);
+                    }
+                }
+            }
+        }
+        
+        return dungeonMap;
+    }
+
+    public static List<String> splitWords(String s) {
+        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    }
+
+    /*
+     4
+	 2147483647 -1 0 2147483647
+	 2147483647 2147483647 2147483647 -1
+	 2147483647 -1 2147483647 -1
+	 0 -1 2147483647 2147483647
+	 
+	 Output:
+	 3 -1 0 1
+	 2 2 1 -1
+	 1 -1 2 -1
+	 0 -1 3 4
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dungeonMapLength = Integer.parseInt(scanner.nextLine());
+        List<List<Integer>> dungeonMap = new ArrayList<>();
+        for (int i = 0; i < dungeonMapLength; i++) {
+            dungeonMap.add(splitWords(scanner.nextLine()).stream().map(Integer::parseInt).collect(Collectors.toList()));
+        }
+        scanner.close();
+        List<List<Integer>> res = mapGateDistances(dungeonMap);
+        for (List<Integer> row : res) {
+            System.out.println(row.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+        }
+    }
+}
---
+++ src/General/Graph/MinimumSpanningTree/Forests.java
@@ -0,0 +1,135 @@
+package General.Graph.MinimumSpanningTree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * The Umbristan Department of Forestry(UDF) is tackling a rather difficult problem and the Umbristan government 
+ * has detached you one of its best workers to go resolve the issue. 
+ * When you arrive you are quickly briefed on the problem at hand. 
+ * Inside the Umbristan National Park there exists an area that has been closed off as fencing needs to be erected in the area. 
+ * The department for whatever reason needs to set up some very expensive fencing between the trees. 
+ * The department has also set up some rules for the fence:
+ * 1 - The fence needs to be set up such that every tree in the area is connected to the fence.
+ * 2 - The department is on a very strict operating budget and so needs to minimize the metres of fencing required.
+ * 3 - The department has counted the number of trees in the area 
+ * 	   as well as determined all possible tree pairs where a fence can be built between the pair.
+ * 
+ * Can you help them figure out the smallest amount of fencing required?
+ * It is possible that not all the nodes will be connected to one another depending on the tree pairs. 
+ * Input will consist of trees the number of trees in the area labelled from 1 to n as well as pairs, 
+ * a list consisting of the tuple [a, b, d] which denotes that a fence can be built between the trees a and b that will be d metres in length.
+ * 
+ * Constraints: 1 <= trees <= 10^5
+ * 
+ * Input: trees = 5, pairs = [[1, 2, 1], [2, 4, 2], [3, 5, 3], [4, 4, 4]]
+ * Output: 6
+ * Explanation: We can erect fencing between trees for the following pairs, 1 and 2, 2 and 4, 3 and 5. 
+ * 			    With this every tree is connected by a fence and we have used 6 metres of fencing.
+ * 
+ * Solution:
+ * 
+ * The main idea is to run Kruskal's algorithm with some tweaked aspects. 
+ * Instead of terminating when n - 1 nodes have been merged we are notified that it may not be possible to merge every node. 
+ * Therefore, we simply just let the algorithm run until every edge is checked. 
+ * One can also run prim's but make sure that every single node is calculated. 
+ * Every connected component needs to have prim's algorithm run on it.
+ */
+public class Forests {
+
+	public static class UnionFind<T> {
+        private Map<T, T> f = new HashMap<>();
+
+        public T find(T x) {
+            T y = f.getOrDefault(x, x);
+            if (y != x) {
+                y = find(y);
+                f.put(x, y);
+            }
+            return y;
+        }
+
+        public void union(T x, T y) {
+            f.put(find(x), find(y));
+        }
+    }
+
+    public static int mstForest(int trees, List<List<Integer>> pairs) {
+         // Sort list, make sure to define custom comparator class cmp to sort edge based on weight from lowest to highest.
+         Collections.sort(pairs, new Comparator<List<Integer>>() {
+        	 public int compare(List<Integer> a, List<Integer> b) {
+        		 return a.get(2).compareTo(b.get(2));
+        	 }
+         });
+         
+         UnionFind<Integer> dsu = new UnionFind<Integer>();
+         int ret = 0;
+         
+         for (int i = 0; i < pairs.size(); i++) {
+             // Check if pairs belong to same set before merging and adding edge to mst (minimum spanning tree).
+             int a = pairs.get(i).get(0);
+             int b = pairs.get(i).get(1);
+             int d = pairs.get(i).get(2);
+             
+             if (dsu.find(a) != dsu.find(b)) {
+                 dsu.union(a, b);
+                 ret += d;
+             }
+         }
+         
+         return ret;
+     }
+
+    public static List<String> splitWords(String s) {
+        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    }
+
+    /*
+     5
+	 4
+	 1 2 1
+	 2 4 2
+	 3 5 3
+	 4 4 4
+	 
+	 Output: 6
+     */
+    /*
+     10
+	 13
+	 1 2 3
+	 2 3 4
+	 10 1 2
+	 5 6 2
+	 1 4 5
+	 7 5 1
+	 9 2 10
+	 7 10 1
+	 8 5 2
+	 5 8 1
+	 6 5 1
+	 4 9 2
+	 7 8 2
+	 
+	 Output: 20
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int trees = Integer.parseInt(scanner.nextLine());
+        int pairsLength = Integer.parseInt(scanner.nextLine());
+        List<List<Integer>> pairs = new ArrayList<>();
+        for (int i = 0; i < pairsLength; i++) {
+            pairs.add(splitWords(scanner.nextLine()).stream().map(Integer::parseInt).collect(Collectors.toList()));
+        }
+        scanner.close();
+        int res = mstForest(trees, pairs);
+        System.out.println(res);
+    }
+}
