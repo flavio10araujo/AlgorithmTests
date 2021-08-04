f00d68b1db30926296aab9a8b992baeac7547d1b
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-08-04 14:13:34 -0400

    OK

--- src/General/Others/ReverseALinkedList.java
+++ src/General/Others/ReverseALinkedList.java
@@ -1,5 +1,7 @@
 package General.Others;
 
+import java.util.Stack;
+
 /**
  * Given pointer to the head node of a linked list, the task is to reverse the linked list. 
  * We need to reverse the list by changing the links between nodes.
@@ -10,7 +12,7 @@ package General.Others;
  * Output: Linked list should be changed to, 
  * 		  4->3->2->1->NULL
  * 
- * Solution:
+ * Solution 01 (with pointers):
  * 
  * Initialize three pointers: 
  * 		prev as NULL, 
@@ -28,45 +30,20 @@ package General.Others;
  * 			prev = curr 
  * 			curr = next
  * 
+ * Solution 02 (with a stack):
+ * 
+ * - Store the nodes (values and address) in the stack until all the values are entered.
+ * - Once all entries are done, update the Head pointer to the last location (i.e the last value).
+ * - Start popping the nodes (value and address) and store them in the same order until the stack is empty.
+ * - Update the next pointer of last Node in the stack by NULL.
+ * 
  * https://www.geeksforgeeks.org/reverse-a-linked-list/
  * 
  */
 public class ReverseALinkedList {
 	static Node head;
 	 
-    static class Node {
-        int data;
-        Node next;
- 
-        Node(int d) {
-            data = d;
-            next = null;
-        }
-    }
- 
-    public Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        Node next = null;
-        
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        
-        return prev;
-    }
- 
-    public void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-    }
- 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
     	ReverseALinkedList list = new ReverseALinkedList();
         
     	list.head = new Node(1);
@@ -78,11 +55,77 @@ public class ReverseALinkedList {
         
         list.printList(head);
         
-        head = list.reverse(head);
+        head = list.reverseWithPointers();
+        //head = list.reverseWithStack();
         
         System.out.println("");
         System.out.println("Reversed linked list:");
         
         list.printList(head);
     }
+    
+    public void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+	
+	public static class Node {
+        int data;
+        Node next;
+ 
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+ 
+    // ### Solution 01 - using pointers - begin ###
+	public Node reverseWithPointers() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+	// ### Solution 01 - end ###
+    
+    // ### Solution 02 - using a stack - begin ###
+    public Node reverseWithStack() {
+        // Create a stack "s" of Node type.
+        Stack<Node> s = new Stack<>();
+        Node temp = head;
+        
+        while (temp.next != null) {
+            // Push all the nodes in to stack.
+            s.add(temp);
+            temp = temp.next;
+        }
+        
+        head = temp;
+       
+        while (!s.isEmpty()) {
+            // Store the top value of stack in list.
+            temp.next = s.peek();
+
+            // Pop the value from stack.
+            s.pop();
+
+            // Update the next pointer in the list.
+            temp = temp.next;
+        }
+        
+        temp.next = null;
+        
+        return head;
+    }
+    // ### Solution 02 - end ###
 }
