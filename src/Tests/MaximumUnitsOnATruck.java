02c7a047f95b1caf1e6a497b77c12b7fa15d954a
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-08-11 20:07:29 -0400

    OK

---
+++ src/Tests/MaximumUnitsOnATruck.java
@@ -0,0 +1,90 @@
+package Tests;
+
+import java.util.Arrays;
+
+/**
+ * You are assigned to put some amount of boxes onto one truck. 
+ * 
+ * You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+ * - numberOfBoxesi is the number of boxes of type i.
+ * - numberOfUnitsPerBoxi is the number of units in each box of the type i.
+ * 
+ * You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. 
+ * You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+ * 
+ * Return the maximum total number of units that can be put on the truck.
+ * 
+ * Example 1:
+ * Input: boxTypes = { {2, 2}, {1, 3}, {3, 1} }, truckSize = 4
+ * Output: 8
+ * Explanation: There are:
+ * 	- 1 box of the first type that contains 3 units.
+ * 	- 2 boxes of the second type that contain 2 units each.
+ *  - 1 box of the third type that contain 1 unit each.
+ *  You can take all the boxes of the first and second types, and one box of the third type.
+ *  The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+ *  
+ *  Example 2:
+ *  Input: boxTypes = {{5,10},{2,5},{4,7},{3,9}}, truckSize = 10
+ *  Output: 91
+ *  
+ *  Constraints:
+ *  1 <= boxTypes.length <= 1000
+ *  1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+ *  1 <= truckSize <= 106
+ *  
+ *  Solution:
+ *  
+ *  Greedy Algorithm.
+ *  The problem is fairly straightforward. 
+ *  We simply sort the boxes by the number of units a box can contain and fill the truck until it's full.
+ */
+public class MaximumUnitsOnATruck {
+
+	public static void main(String[] args) {
+		int[][] boxTypes = { {2, 2}, {1, 3}, {3, 1} };
+		int truckSize = 4;
+		
+		System.out.println(getMaximumUnitsOnATruck(boxTypes, truckSize));
+	}
+	
+	public static int getMaximumUnitsOnATruck(int[][] boxTypes, int truckSize) {
+		
+		/*for(int i = 0; i < boxTypes.length; i++) {
+			for(int j = 0; j < 2; j++) {
+				System.out.print(boxTypes[i][j]);
+			}
+			System.out.println("");
+		}*/
+		
+		// First thing is to order the list by unit. Biggest units first.
+		Arrays.sort(boxTypes, (a, b) -> -Integer.compare(a[1], b[1]));
+		
+		/*for(int i = 0; i < boxTypes.length; i++) {
+			for(int j = 0; j < 2; j++) {
+				System.out.print(boxTypes[i][j]);
+			}
+			System.out.println("");
+		}*/
+		
+		int maximumUnitsOnATruck = 0;
+		int countBoxes = 0;
+		
+		label : {
+			for (int i = 0; i < boxTypes.length; i++) {
+				for (int j = 1; j <= boxTypes[i][0]; j++) {
+					
+					maximumUnitsOnATruck += boxTypes[i][1];
+					
+					countBoxes++;
+					
+					if (countBoxes >= truckSize) {
+						break label;
+					}
+				}
+			}
+		}
+		
+		return maximumUnitsOnATruck;
+	}
+}
---
+++ src/Tests/SlowestKey.java
@@ -0,0 +1,102 @@
+package Tests;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+ * 
+ * You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, 
+ * and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. 
+ * 
+ * Both arrays are 0-indexed. 
+ * The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+ * 
+ * The tester wants to know the key of the keypress that had the longest duration. 
+ * The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+ * 
+ * Note that the same key could have been pressed multiple times during the test, 
+ * and these multiple presses of the same key may not have had the same duration.
+ * 
+ * Return the key of the keypress that had the longest duration. 
+ * If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+ * 
+ * Example 1:
+ * 
+ * Input: releaseTimes = [9,29,49,50], keysPressed = "cbcd"
+ * Output: "c"
+ * Explanation: 
+ * 	The keypresses were as follows:
+ * 		Keypress for 'c' had a duration of 9 (pressed at time 0 and released at time 9).
+ * 		Keypress for 'b' had a duration of 29 - 9 = 20 (pressed at time 9 right after the release of the previous character and released at time 29).
+ * 		Keypress for 'c' had a duration of 49 - 29 = 20 (pressed at time 29 right after the release of the previous character and released at time 49).
+ * 		Keypress for 'd' had a duration of 50 - 49 = 1 (pressed at time 49 right after the release of the previous character and released at time 50).
+ * The longest of these was the keypress for 'b' and the second keypress for 'c', both with duration 20.
+ * 'c' is lexicographically larger than 'b', so the answer is 'c'.
+ * 
+ * Example 2:
+ * 
+ * Input: releaseTimes = [12,23,36,46,62], keysPressed = "spuda"
+ * Output: "a"
+ * Explanation: 
+ * 	The keypresses were as follows:
+ * 		Keypress for 's' had a duration of 12.
+ * 		Keypress for 'p' had a duration of 23 - 12 = 11.
+ * 		Keypress for 'u' had a duration of 36 - 23 = 13.
+ * 		Keypress for 'd' had a duration of 46 - 36 = 10.
+ * 		Keypress for 'a' had a duration of 62 - 46 = 16.
+ * The longest of these was the keypress for 'a' with duration 16.
+ * 
+ * Constraints:
+ * releaseTimes.length == n
+ * keysPressed.length == n
+ * 2 <= n <= 1000
+ * 1 <= releaseTimes[i] <= 109
+ * releaseTimes[i] < releaseTimes[i+1]
+ * keysPressed contains only lowercase English letters.
+ */
+public class SlowestKey {
+
+	public static void main(String[] args) {
+		int[] releaseTimes = {9,29,49,50};
+		String keysPressed = "cbcd";
+		System.out.println(slowestKey(releaseTimes, keysPressed));
+	}
+
+	public static char slowestKey(int[] releaseTimes, String keysPressed) {
+        
+		if (releaseTimes.length == 0 || keysPressed == null || keysPressed.length() != releaseTimes.length) {
+			return ' ';
+		}
+		
+		List<Character> slowest = new ArrayList<>();
+		int slowInt = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < releaseTimes.length; i++) {
+			
+			if (i == 0) {
+				slowInt = releaseTimes[0];
+				slowest.add(keysPressed.charAt(0));
+			} else {
+				int time = releaseTimes[i] - releaseTimes[i - 1];
+				
+				if (time > slowInt) {
+					slowInt = time;
+					slowest.clear();
+					slowest.add(keysPressed.charAt(i));
+				} else if (time == slowInt) {
+					slowest.add(keysPressed.charAt(i));
+				}
+			}
+		}
+		
+		if (slowest.size() == 1) {
+			return slowest.get(0);
+		}
+		
+		Collections.sort(slowest);
+		
+		return slowest.get(slowest.size() - 1);
+    }
+}
