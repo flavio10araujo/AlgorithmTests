55d4103d2b3d37b2b80e99d92cdd935e66ad71c1
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-10-29 13:05:05 -0400

    OK

--- src/General/Backtracking/WordSearch.java
+++ src/General/Backtracking/WordSearch.java
@@ -1,7 +1,6 @@
 package General.Backtracking;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -40,19 +39,45 @@ import java.util.Set;
  */
 public class WordSearch {
 	
-	public static boolean exist(List<List<String>> board, String word) {    
-        int height = board.size();
-        int width = board.get(0).size();
-        Character firstLetter = word.charAt(0);
+	public static void main(String[] args) {
+		//char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+		//String word = "ABCCED";
+		
+		char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+		String word = "ABCB";
+		
+    	System.out.println(solution01(board, word));
+    	
+    	System.out.println(solution02(board, word));
+    }
+	
+	static int[][] delta = new int[4][2];
+    
+    /**
+     * 
+     * @param board
+     * @param word
+     * @return
+     */
+	public static boolean solution01(char[][] board, String word) {
         
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                if (board.get(y).get(x).equalsIgnoreCase(firstLetter.toString())) {
+        char firstLetter = word.charAt(0);
+        
+        int height = board.length;
+        int width = board[0].length;
+        
+        initializeDelta();
+        
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (board[row][col] == firstLetter) {
+                    Set<Coordinate> coords = new HashSet<>();
+                    coords.add(new Coordinate(row, col));
                     
-                	Set<String> mySet = new HashSet<String>();
-                	mySet.add(x+","+y);
-                	
-                	if (wordFinder(board, word, x, y, List.of(firstLetter.toString()), mySet)) {
+                    List<Character> chars = new ArrayList<>();
+                    chars.add(word.charAt(0));
+                    
+                    if (wordFinder(board, word, row, col, coords, 0, chars)) {
                         return true;
                     }
                 }
@@ -62,123 +87,199 @@ public class WordSearch {
         return false;
     }
     
-	public static boolean compareListStringAndString(List<String> path, String word) {
-		String str = new String("");
-		
-		for (String s : path) {
-			str += s;
-		}
-		
-		if (str.equalsIgnoreCase(word)) {
-			return true;
-		} 
-		
-		return false;
-	} 
-
-    public static boolean wordFinder(List<List<String>> board, String word, int x, int y, List<String> path, Set<String> mySet) {
+    public static void initializeDelta() {
+        // left
+        delta[0][0] = 0;
+        delta[0][1] = -1;
+        // up
+        delta[1][0] = -1;
+        delta[1][1] = 0;
+        // right
+        delta[2][0] = 0;
+        delta[2][1] = 1;
+        // down
+        delta[3][0] = 1;
+        delta[3][1] = 0;
+    }
+    
+    public static boolean wordFinder(char[][] board, String word, int row, int col, Set<Coordinate> coords, int indexWord, List<Character> chars) {
         
-    	System.out.println("path="+path);
-    	System.out.println("mySet"+mySet);
-    	
-    	if (path.size() == word.length()) {
-    		if (compareListStringAndString(path, word)) {
+        if (chars.size() == word.length()) {
+            return true;
+        }
+        
+        int indexDelta, rowFuture, colFuture;
+        
+        indexWord++;
+        
+        // left
+        indexDelta = 0;
+        rowFuture = row + delta[indexDelta][0];
+        colFuture = col + delta[indexDelta][1];
+        if (isValidMove(board, word, rowFuture, colFuture, coords, indexWord, indexDelta)) {
+            
+        	Coordinate c = new Coordinate(rowFuture, colFuture);
+            coords.add(c);
+            chars.add(word.charAt(indexWord));
+            
+            if (wordFinder(board, word, rowFuture, colFuture, coords, indexWord, chars)) {
                 return true;
             }
             
-            return false;
-        }
-    	
-    	// going right
-        if (x < board.get(0).size() - 1 && !mySet.contains((x+1)+","+y)) {
-        	x = x + 1;
-            
-            path = new ArrayList<String>(path);
-            path.add(board.get(y).get(x));
-            mySet.add(x+","+y);
-            
-            if (wordFinder(board, word, x, y, path, mySet)) {
-            	return true;
-            }
-            
-            path.remove(path.size() - 1);
-            mySet.remove(x+","+y);
-            
-            x = x - 1;
+            chars.remove(chars.size() - 1);
+            coords.remove(c);
         }
         
-        // going left
-        if (x > 0 && !mySet.contains((x-1)+","+y)) {
-        	x = x - 1;
-        	
-            path = new ArrayList<String>(path);
-            path.add(board.get(y).get(x));
-            mySet.add(x+","+y);
-
-            if (wordFinder(board, word, x, y, path, mySet)) {
-            	return true;
+        // up
+        indexDelta = 1;
+        rowFuture = row + delta[indexDelta][0];
+        colFuture = col + delta[indexDelta][1];
+        if (isValidMove(board, word, rowFuture, colFuture, coords, indexWord, indexDelta)) {
+            
+        	Coordinate c = new Coordinate(rowFuture, colFuture);
+            coords.add(c);
+            chars.add(word.charAt(indexWord));
+            
+            if (wordFinder(board, word, rowFuture, colFuture, coords, indexWord, chars)) {
+                return true;
             }
             
-            path.remove(path.size() - 1);
-            mySet.remove(x+","+y);
-            
-            x = x + 1;
+            chars.remove(chars.size() - 1);
+            coords.remove(c);
         }
         
-        // going down
-        if (y < board.size() - 1 && !mySet.contains(x+","+(y+1))) {
-            y = y + 1;
+        // right
+        indexDelta = 2;
+        rowFuture = row + delta[indexDelta][0];
+        colFuture = col + delta[indexDelta][1];
+        if (isValidMove(board, word, rowFuture, colFuture, coords, indexWord, indexDelta)) {
             
-            path = new ArrayList<String>(path);
-            path.add(board.get(y).get(x));
-            mySet.add(x+","+y);
+        	Coordinate c = new Coordinate(rowFuture, colFuture);
+            coords.add(c);
+            chars.add(word.charAt(indexWord));
             
-            if (wordFinder(board, word, x, y, path, mySet)) {
-            	return true;
+            if (wordFinder(board, word, rowFuture, colFuture, coords, indexWord, chars)) {
+                return true;
             }
             
-            path.remove(path.size() - 1);
-            mySet.remove(x+","+y);
-            
-            y = y - 1;
+            chars.remove(chars.size() - 1);
+            coords.remove(c);
         }
         
-        // going up
-        if (y > 0 && !mySet.contains(x+","+(y-1))) {
-        	y = y - 1;
+        // down
+        indexDelta = 3;
+        rowFuture = row + delta[indexDelta][0];
+        colFuture = col + delta[indexDelta][1];
+        if (isValidMove(board, word, rowFuture, colFuture, coords, indexWord, indexDelta)) {
             
-        	path = new ArrayList<String>(path);
-            path.add(board.get(y).get(x));
-            mySet.add(x+","+y);
+        	Coordinate c = new Coordinate(rowFuture, colFuture);
+            coords.add(c);
+            chars.add(word.charAt(indexWord));
             
-            if (wordFinder(board, word, x, y, path, mySet)) {
-            	return true;
+            if (wordFinder(board, word, rowFuture, colFuture, coords, indexWord, chars)) {
+                return true;
             }
             
-            path.remove(path.size() - 1);
-            mySet.remove(x+","+y);
-            
-            y = y + 1;
+            chars.remove(chars.size() - 1);
+            coords.remove(c);
         }
         
         return false;
     }
-
-    public static List<String> splitWords(String s) {
-        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    
+    public static boolean isValidMove(char[][] board, String word, int rowFuture, int colFuture, Set<Coordinate> coords, int indexWord, int indexDelta) {
+        
+        if (rowFuture < 0 || colFuture < 0 || rowFuture >= board.length || colFuture >= board[0].length) {
+            return false;
+        }
+        
+        if (board[rowFuture][colFuture] != word.charAt(indexWord)) {
+        	return false;
+        }
+        
+        if (coords.contains(new Coordinate(rowFuture, colFuture))) {
+        	return false;
+        }
+        
+        return true;
+    }
+    
+    static class Coordinate {
+        private int row;
+        private int col;
+        
+        public Coordinate(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+        
+        public int getRow() {
+            return this.row;
+        }
+        
+        public int getCol() {
+            return this.col;
+        }
+        
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Coordinate))
+    			return false;	
+    		if (obj == this)
+    			return true;
+    		
+    		if (this.row == ((Coordinate) obj).row 
+    				&& this.col == ((Coordinate) obj).col) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        public int hashCode() {
+        	if (this.row == 0) {
+        		return this.col;
+        	}
+        	
+        	return (this.row * 10) + this.col;
+        }
     }
 
-    public static void main(String[] args) {
-        List<List<String>> board = new ArrayList<>();
-        List<String> l1 = List.of("A", "B", "C", "E");
-        List<String> l2 = List.of("S", "F", "C", "S"); // S F C S
-        List<String> l3 = List.of("A", "D", "E", "E"); // A D E E
-        board.add(l1);
-        board.add(l2);
-        board.add(l3);
-        String word = "EESCFDA";
-        
-        boolean res = exist(board, word);
-        System.out.println(res);
+    /**
+     * 
+     * @param board
+     * @param word
+     * @return
+     */
+    public static boolean solution02(char[][] board, String word) {
+    	for (int i = 0; i < board.length; i++) {
+    		for (int j = 0; j < board[i].length; j++) {
+    			if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)) {
+    				return true;
+    			}
+    		}
+    	}
+    	
+    	return false;
+    }
+    
+    public static boolean dfs(char[][] board, int i, int j, int count, String word) {
+    	if (count == word.length()) {
+    		return true;
+    	}
+    	
+    	if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(count)) {
+    		return false;
+    	}
+    	
+    	char temp = board[i][j];
+    	board[i][j] = ' ';
+    	
+    	boolean found = dfs(board, i + 1, j, count + 1, word)
+    			|| dfs(board, i - 1, j, count + 1, word)
+    			|| dfs(board, i, j + 1, count + 1, word)
+    			|| dfs(board, i, j - 1, count + 1, word);
+    	
+    	board[i][j] = temp;
+    	return found;
     }
 }
