d1366b3008d9ca72c86ecfb6110c3dfd1286281f
flavio10araujo <flavio10araujo@yahoo.com.br>
2022-03-07 09:40:54 -0500

    OK

--- src/General/Backtracking/WordPatternII.java
+++ src/General/Backtracking/WordPatternII.java
@@ -20,49 +20,21 @@ import java.util.Set;
  */
 public class WordPatternII {
 	
-	public static boolean helper(String pattern, String s, int i, int j, Map<Character, String> map, Set<String> set) {
-
-		if (i == pattern.length() && j == s.length()) {
-			return true;
-		}
-
-		if (i >= pattern.length() || j >= s.length()) {
-			return false;
-		}
-
-		char c = pattern.charAt(i);
-		
-		for (int k = j + 1; k <= s.length(); k++) {
-			
-			String sub = s.substring(j, k);
-			
-			System.out.println("c="+c+" k="+k+" j="+j+" i="+i+" sub="+sub);
-			
-			if (!map.containsKey(c) && !set.contains(sub)) {
-				map.put(c, sub);
-				set.add(sub);
-				
-				System.out.println("map="+map+" set="+set);
-				
-				if (helper(pattern, s, i + 1, k, map, set)) {
-					return true;
-				}
-				
-				map.remove(c);
-				set.remove(sub);
-				
-				System.out.println("map="+map+" set="+set);
-				
-			} else if (map.containsKey(c) && map.get(c).equals(sub)) {
-				if (helper(pattern, s, i + 1, k, map, set)) {
-					return true;
-				}
-			}
-		}
-		
-		return false;
+	public static void main(String[] args) {
+		//long startTime = System.nanoTime();
+    	
+		//String pattern = "ABAB"; String s = "redblueredblue";
+		//String pattern = "ABAB"; String s = "XYXY"; // true
+		String pattern = "ABAB"; String s = "XXXX"; // false
+        
+		System.out.println(wordPatternMatch(pattern, s));
+        
+        //long endTime = System.nanoTime();
+        //long timeElapsed = endTime - startTime;
+        //System.out.println("Execution time in nanoseconds: " + timeElapsed);
+        //System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
 	}
-
+	
 	/**
 	 * Time complexity: O(pattern.length ^ s.length()).
 	 * Space complexity: 
@@ -84,16 +56,40 @@ public class WordPatternII {
 		
 		return helper(pattern, s, 0, 0, map, set);
 	}
+	
+	public static boolean helper(String pattern, String s, int i, int j, Map<Character, String> map, Set<String> set) {
 
-	public static void main(String[] args) {
-		long startTime = System.nanoTime();
-    	String pattern = "ABAB";
-        String s = "redblueredblue";
-        boolean res = wordPatternMatch(pattern, s);
-        System.out.println(res);
-        long endTime = System.nanoTime();
-        long timeElapsed = endTime - startTime;
-        System.out.println("Execution time in nanoseconds: " + timeElapsed);
-        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
+		if (i == pattern.length() && j == s.length()) {
+			return true;
+		}
+
+		if (i >= pattern.length() || j >= s.length()) {
+			return false;
+		}
+
+		char c = pattern.charAt(i);
+		
+		for (int k = j + 1; k <= s.length(); k++) {
+			
+			String sub = s.substring(j, k);
+			
+			if (!map.containsKey(c) && !set.contains(sub)) {
+				map.put(c, sub);
+				set.add(sub);
+				
+				if (helper(pattern, s, i + 1, k, map, set)) {
+					return true;
+				}
+				
+				map.remove(c);
+				set.remove(sub);
+			} else if (map.containsKey(c) && map.get(c).equals(sub)) {
+				if (helper(pattern, s, i + 1, k, map, set)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
